c69fb833afa7cb198c4bad7521821a55353a9ee5
fujicho <nnnn.sohei.fujita@gmail.com>
2020-09-22 10:38:41 +0900

    add a current_teacher_member method

---
+++ app/controllers/teacher/base.rb
@@ -0,0 +1,10 @@
+class Teacher::Base < ApplicationController
+  private def current_teacher_member
+    if session[:teacher_member_id]
+      @current_teacher_member || =
+        TeacherMember.find_by(id: session[:teacher_member_id])
+    end
+  end
+
+  helper_method :current_teacher_member
+end
