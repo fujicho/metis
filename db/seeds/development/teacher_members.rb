58c2ff68396dd7a96ba7ede46ba06d8825f5a0e3
fujicho <nnnn.sohei.fujita@gmail.com>
2020-09-22 10:38:14 +0900

    seedデータの投入

---
+++ db/seeds/development/teacher_members.rb
@@ -0,0 +1,9 @@
+TeacherMember.create!(
+  email: "shimizu@example.com",
+  family_name: "清水",
+  given_name: "勝",
+  family_name_kana: "shimizu",
+  given_name_kana: "masaru",
+  password: "password",
+  start_date: Date.today
+)
