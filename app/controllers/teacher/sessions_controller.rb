789d875d0564a7d15d2654215fc756c282eda9bc
fujicho <nnnn.sohei.fujita@gmail.com>
2020-09-24 18:10:48 +0900

    add new,create action

--- app/controllers/teacher/sessions_controller.rb
+++ app/controllers/teacher/sessions_controller.rb
@@ -1,2 +1,24 @@
-class Teacher::SessionsController < ApplicationController
+class Teacher::SessionsController < Teacher::Base
+  def new
+    if current_teacher_member
+      redirect_to :teacher_root
+    else
+      @form = Teacher::LoginForm.new
+      render aciton: "new"
+    end
+  end
+
+  def create
+    @form = Teacher::LoginForm.new(params[:teacher_login_form])
+    if @form.email.present?
+      teacher_member =
+        TeacherMember.find_by("LOWER(email) = ?", @form.email.downcase)
+    end
+    if teacher_member
+      session[:teacher_member_id] = teacher_member.id
+      redirect_to :teacher_root
+    else
+      render action: "new"
+    end
+  end
 end
