500f0cace41ea021981d4f231c90f5e8dfe08c61
fujicho <nnnn.sohei.fujita@gmail.com>
2020-09-29 17:42:44 +0900

    "文字列を与えるとhashed_passwordは長さ60の文字列になる,"nilを与えると、hashed_passwordはnilになるのテスト

--- spec/models/administrator_spec.rb
+++ spec/models/administrator_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Administrator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#password=" do
+    example "文字列を与えるとhashed_passwordは長さ60の文字列になる" do
+      member = Administrator.new
+      member.password = "password"
+      expect(member.hashed_password).to be_kind_of(String)
+      expect(member.hashed_password.size).to eq(60)
+    end
+
+    example "nilを与えると、hashed_passwordはnilになる"do
+      member = Administrator.new(hashed_password: "fugafuga")
+      member.password = nil
+      expect(member.hashed_password).to be_nil
+    end
+  end
 end
