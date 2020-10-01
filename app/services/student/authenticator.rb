c6a54b32c4e6833698f43fba1aa15a51e2efc095
fujicho <nnnn.sohei.fujita@gmail.com>
2020-10-01 18:39:56 +0900

    Teacher::Authenticatorの作成

---
+++ app/services/student/authenticator.rb
@@ -0,0 +1,14 @@
+class Teacher::Authenticator
+  def initialize(student_member)
+    @student_member = student_member
+  end
+
+  def authenticate(raw_password)
+    @student_member &&
+      !@student_member.suspended? &&
+      @student_member.hashed_password &&
+      @student_member.start_date <= Date.today &&
+      @student_member.graduate_date > Date.today &&
+      BCrypt::Password.new(@student_member.hashed_password) == raw_password
+  end
+end
