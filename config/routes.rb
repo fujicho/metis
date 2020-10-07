e3b631d4e5762ae31ef789c768d9422f86f9f314
fujicho <nnnn.sohei.fujita@gmail.com>
2020-10-07 22:40:30 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     get "login" => "sessions#new", as: :login
     post "session" => "sessions#create", as: :session
     delete "session" => "sessions#destroy"
+    resources :student_members
   end
 
   namespace :student do
