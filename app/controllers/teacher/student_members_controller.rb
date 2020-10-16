10a87b24cc429bdaf4ce9339b8af922e4fc0dd53
fujicho <nnnn.sohei.fujita@gmail.com>
2020-10-16 21:07:08 +0900

    add create , update

--- app/controllers/teacher/student_members_controller.rb
+++ app/controllers/teacher/student_members_controller.rb
@@ -15,4 +15,24 @@ class Teacher::StudentMembersController < Teacher::Base
   def edit
     @student_member = StudentMember.find(params[:id])
   end
+
+  def create
+    @student_member = StudentMember.new(params[:student_member])
+    if @student_member.save
+      flash.notice = "学生アカウントを新規登録しました。"
+      redirect_to :teacher_student_members
+    else
+      render action: "new"
+    end
+  end
+
+  def update
+    @student_member = StaffMember.find(params[:id])
+    @student_member.assign_attributes(params[:student_member])
+    if @student_member.save
+      redirect_to :teacher_student_members
+    else
+      render action: "edit"
+    end
+  end
 end
