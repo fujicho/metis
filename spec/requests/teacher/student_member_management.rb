d33508a965ee14e2d904dd9eb29ae199a190687c
fujicho <nnnn.sohei.fujita@gmail.com>
2020-10-17 23:00:54 +0900

    add a example "生徒一覧にリダイレクト"

---
+++ spec/requests/teacher/student_member_management.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "教職員による生徒管理" do
+  let(:teacher_member){ create(:teacher_member)}
+
+  describe "新規登録" do
+    let(:params_hash){ attributes_for(:student_member)}
+
+    example "生徒一覧にリダイレクト" do
+      post teacher_student_members_url, params:{ student_member: params_hash}
+      expect(response).to redirect_to(teacher_student_members_url)
+    end
+  end
+
+  # describe "更新" do
+  #   let(:student_member){ create(:student_member)}
+  #   let(:params_hash){ attributes_for(:student_member)}
+
+
+  # end
+end
