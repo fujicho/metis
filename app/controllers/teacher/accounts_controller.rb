8f81a9f65932653a4f759d7471761bb7cd9b235e
fujicho <nnnn.sohei.fujita@gmail.com>
2020-10-19 10:35:10 +0900

    add private teacher_member_params

--- app/controllers/teacher/accounts_controller.rb
+++ app/controllers/teacher/accounts_controller.rb
@@ -6,4 +6,9 @@ class Teacher::AccountsController < Teacher::Base
   def edit
     @teacher_member = current_teacher_member
   end
+
+  private def teacher_member_params
+    params.require(:teacher_member).permit( :email, :family_name, :given_name, 
+      :family_name_kana, :given_name_kana)
+  end
 end
