25df1ddff98781fbe5484dbc86d4b18a41ed4d87
fujicho <nnnn.sohei.fujita@gmail.com>
2020-10-19 11:23:29 +0900

    add a update action

--- app/controllers/teacher/accounts_controller.rb
+++ app/controllers/teacher/accounts_controller.rb
@@ -7,6 +7,16 @@ class Teacher::AccountsController < Teacher::Base
     @teacher_member = current_teacher_member
   end
 
+  def update
+    @teacher_member = current_teacher_member
+    @teacher_member.assign_attributes(teacher_member_params)
+    if @teacher_member.save
+      redirect_to :teacher_account
+    else
+      render action: "edit"
+    end
+  end
+
   private def teacher_member_params
     params.require(:teacher_member).permit( :email, :family_name, :given_name, 
       :family_name_kana, :given_name_kana)
