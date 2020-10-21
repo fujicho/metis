1195e5a52d71e77ea5243dabbc8c2db8e2ac60a5
fujicho <nnnn.sohei.fujita@gmail.com>
2020-10-21 17:03:18 +0900

    check_account, check_timeoutメソッドの定義

--- app/controllers/teacher/base.rb
+++ app/controllers/teacher/base.rb
@@ -1,5 +1,7 @@
 class Teacher::Base < ApplicationController
   before_action :authorize
+  before_action :check_account
+  before_action :check_timeout
 
   private def authorize
     unless current_teacher_member
@@ -10,6 +12,26 @@ class Teacher::Base < ApplicationController
     if session[:teacher_member_id]
       @current_teacher_member ||=
         TeacherMember.find_by(id: session[:teacher_member_id])
+    end
+  end
+
+  private def check_account
+    if current_teacher_member && !current_teacher_member.active?
+      redirect_to :teacher_root
+    end
+  end
+
+
+  TIMEOUT = 60.minutes
+
+  private def check_timeout
+    if current_teacher_member 
+      if session[:last_access_time] >= TIMEOUT.ago
+        session[:last_access_time] = Time.current
+      else
+        session.delete(:teacher_member_id)
+        redirect_to :teahcer_login
+      end
     end
   end
 
