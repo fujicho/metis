df3d4bda1a21efbf60b48b8842c00f16170a2331
fujicho <nnnn.sohei.fujita@gmail.com>
2020-10-25 19:19:43 +0900

    Update my_account_management_spec.rb

--- spec/requests/teacher/my_account_management_spec.rb
+++ spec/requests/teacher/my_account_management_spec.rb
@@ -15,6 +15,21 @@ describe "教職員による自身のアカウント管理" do
       }
   end
 
+  describe "情報表示" do
+    let(:teacher_member){create(:teacher_member)}
+
+    example "成功" do
+      get teacher_account_url
+      expect(response.status).to eq(200)
+    end
+
+    example "停止フラグがセットされたら強制的にログアウト" do
+      teacher_member.update_column(:suspended, true)
+      get teacher_account_url
+      expect(response).to redirect_to(:teacher_root)
+    end
+  end
+
   describe "更新" do
     let(:params_hash){attributes_for(:teacher_member)}
     let(:teacher_member){create(:teacher_member)}
