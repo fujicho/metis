8952771b5f779aea1efd294c6544dfef174a464b
fujicho <nnnn.sohei.fujita@gmail.com>
2020-10-27 23:04:20 +0900

    Update student_event.rb

--- app/models/student_event.rb
+++ app/models/student_event.rb
@@ -1,2 +1,6 @@
 class StudentEvent < ApplicationRecord
+  self.inheritance_column = nil
+
+  belongs_to :member, class_name: "StudentMember", foreign_key: "student_member_id"
+  alias_attribute :occurred_at, :created_at
 end
