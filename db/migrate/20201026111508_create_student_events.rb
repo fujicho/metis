b40bf9a54666a6866d2b2f7c9fa8a91129d644ed
fujicho <nnnn.sohei.fujita@gmail.com>
2020-10-27 22:47:57 +0900

    Update 20201026111508_create_student_events.rb

--- db/migrate/20201026111508_create_student_events.rb
+++ db/migrate/20201026111508_create_student_events.rb
@@ -1,8 +1,12 @@
 class CreateStudentEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :student_events do |t|
-
-      t.timestamps
+      t.references :student_member, null: false, index: false, foreign_key: true
+      t.string :type, null: false
+      t.datetime :created_at, null: false
     end
+    
+    add_index :student_events, :created_at
+    add_index :student_events,[ :student_member_id, :created_at ]
   end
 end
