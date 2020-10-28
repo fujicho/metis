d2acd8517475d070205cebe4f9aee983a248cefd
fujicho <nnnn.sohei.fujita@gmail.com>
2020-10-28 20:24:15 +0900

    Update sessions_controller.rb

--- app/controllers/student/sessions_controller.rb
+++ app/controllers/student/sessions_controller.rb
@@ -1,4 +1,6 @@
 class Student::SessionsController < Student::Base
+  skip_before_action :authorize
+
   def new
     if current_student_member
       redirect_to :student_root
@@ -9,13 +11,15 @@ class Student::SessionsController < Student::Base
   end
 
   def create
-    @form = Student::LoginForm.new(params[:student_login_form])
+    @form = Student::LoginForm.new(login_form_params)
     if @form.student_number.present?
       student_member =
         StudentMember.find_by(student_number: @form.student_number)
     end
     if Student::Authenticator.new(student_member).authenticate(@form.password)
       session[:student_member_id] = student_member.id
+      session[:last_access_time] = Time.current
+      student_member.events.create!(type: "logged_in")
       redirect_to :student_root
     else
       render action: "new"
@@ -23,7 +27,14 @@ class Student::SessionsController < Student::Base
   end
   
   def destroy
+    if current_student_member
+      current_student_member.events.create!(type: "logged_out")
+    end
     session.delete(:student_member_id)
     redirect_to :student_root
+  end
+
+  private def login_form_params
+    params.require(:student_login_form).permit(:student_number, :password)
   end
 end
