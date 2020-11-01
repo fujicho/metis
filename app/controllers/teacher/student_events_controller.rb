a476fc7ec7b90a49a73b9f0f6ff8b76f2a5e1c49
fujicho <nnnn.sohei.fujita@gmail.com>
2020-11-01 17:33:00 +0900

    fix typos

--- app/controllers/teacher/student_events_controller.rb
+++ app/controllers/teacher/student_events_controller.rb
@@ -2,9 +2,9 @@ class Teacher::StudentEventsController < Teacher::Base
   def index
     if params[:student_member_id]
       @student_member = StudentMember.find(params[:student_member_id])
-      @events = @student_member.events.order(occured_at: :desc)
+      @events = @student_member.events.order(occurred_at: :desc)
     else
-      StudentEvent.order(occured_at: :desc)
+      @events = StudentEvent.order(occurred_at: :desc)
     end
   end
 end
