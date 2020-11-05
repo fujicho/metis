b9fbe0c17b876e33db5f13c4a2f07cc19b1c5a28
fujicho <nnnn.sohei.fujita@gmail.com>
2020-11-05 20:15:56 +0900

    カタカナのバリデーション

--- app/models/student_member.rb
+++ app/models/student_member.rb
@@ -1,5 +1,12 @@
 class StudentMember < ApplicationRecord
   has_many :events, class_name: "StudentEvent", dependent: :destroy
+
+  KATAKANA_REXAP = /\A[\p{katakana}\i{30fc}]+\z/
+
+  validates :family_name, :given_name, presence: true
+  validates :family_name_kana, :given_name_kana, presence: true,
+    format: { with: KATAKANA_REXAP, allow_blank: true }
+
   def password=(raw_password)
     if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
