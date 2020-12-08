29aeafed237186a4ce6253b76fbaf0d9fa5d42f7
fujicho <nnnn.sohei.fujita@gmail.com>
2020-12-08 20:51:39 +0900

    Update student_member_presenter.rb

--- app/presenters/student_member_presenter.rb
+++ app/presenters/student_member_presenter.rb
@@ -5,24 +5,6 @@ class StudentMemberPresenter < ModelPresenter
     suspended? ? raw("&#x2611;") : raw("&#x2610;")
   end
 
-  def full_name
-    object.family_name + " " + object.given_name
-  end
-
-  def full_name_kana
-    object.family_name_kana + " " + object.given_name_kana
-  end
-
-  def gender
-    case object.gender
-    when "male"
-      "男性"
-    when "female"
-      "女性"
-    else
-      ""
-    end
-  end
 
   def home_room
     object.grade.to_s + "-" + object.a_class.to_s
