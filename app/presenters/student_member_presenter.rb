01f226d343677779d4268bc9ec5b218caa21c9e9
fujicho <nnnn.sohei.fujita@gmail.com>
2020-12-08 16:47:10 +0900

    Update student_member_presenter.rb

--- app/presenters/student_member_presenter.rb
+++ app/presenters/student_member_presenter.rb
@@ -1,7 +1,31 @@
 class StudentMemberPresenter < ModelPresenter
-  delegate :suspended?, to: :object
+  delegate :suspended?,:email, to: :object
 
   def suspended_mark
     suspended? ? raw("&#x2611;") : raw("&#x2610;")
   end
+
+  def full_name
+    object.family_name + " " + object.given_name
+  end
+
+  def full_name_kana
+    object.family_name_kana + " " + object.given_name_kana
+  end
+
+  def gender
+    case object.gender
+    when "male"
+      "男性"
+    when "female"
+      "女性"
+    else
+      ""
+    end
+  end
+
+  def home_room
+    object.grade.to_s + "-" + object.a_class.to_s
+  end
+
 end
