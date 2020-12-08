2f0daf0a82016708c3f2aad7f80e680b44e5fa6e
fujicho <nnnn.sohei.fujita@gmail.com>
2020-12-08 20:51:35 +0900

    Update model_presenter.rb

--- app/presenters/model_presenter.rb
+++ app/presenters/model_presenter.rb
@@ -6,4 +6,28 @@ class ModelPresenter
     @object = object
     @view_context = view_context
   end
+
+  def full_name
+    object.family_name + " " + object.given_name
+  end
+
+  def full_name_kana
+    object.family_name_kana + " " + object.given_name_kana
+  end
+
+  def gender
+    case object.gender
+    when "male"
+      "男性"
+    when "female"
+      "女性"
+    else
+      ""
+    end
+  end
+
+  def birth_day
+    object.birth_day.strftime("%Y/%m/%d")
+  end
+
 end
