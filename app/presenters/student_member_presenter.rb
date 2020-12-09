954315dc56bb3679bec8839f6ddb0d224367ec87
fujicho <nnnn.sohei.fujita@gmail.com>
2020-12-09 22:19:56 +0900

    Update student_member_presenter.rb

--- app/presenters/student_member_presenter.rb
+++ app/presenters/student_member_presenter.rb
@@ -1,13 +1,20 @@
 class StudentMemberPresenter < ModelPresenter
-  delegate :suspended?,:email, to: :object
+  delegate :suspended?,:email,:student_number, :homeroom_teacher,
+    :emergency_contact, :telephone_number, :homeroom_teacher, to: :object
 
   def suspended_mark
     suspended? ? raw("&#x2611;") : raw("&#x2610;")
   end
 
-
   def home_room
     object.grade.to_s + "-" + object.a_class.to_s
   end
 
+  def start_date
+    object.start_date.strftime("%Y/%m/%d")
+  end
+
+  def graduation_date
+    object.graduation_date.try(:strftime,"%Y/%m/%d")
+  end
 end
