ff00b4c54e843c7b1abde1a6344e08637cb6c38a
fujicho <nnnn.sohei.fujita@gmail.com>
2020-12-20 14:14:06 +0900

    Update form_presenter.rb

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -36,4 +36,20 @@ class FormPresenter
       m << error_messages_for(name)
     end
   end
+
+  def drop_down_list_block(name, label_text, choices, options = {})
+    markup(:div, class: "input-block") do |m|
+      m << label(name,label_text, class: options[:required] ? "required" : nil)
+      m << form_builder.select(name, choices, {include_blank: true}, options)
+      m << error_messages_for(name)
+    end
+  end
+  
+  def postal_code_block(name,label_text,options)
+    markup(:div,class: "input-block") do |m|
+      m << label(name, label_text, class: options[:required] ? "required" : nil)
+      m << text_field(name,options)
+      m << error_messages_for(name)
+    end
+  end
 end
