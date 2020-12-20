1da1632617dc63cac46bbd77e1a318cbb250240b
fujicho <nnnn.sohei.fujita@gmail.com>
2020-12-20 17:08:03 +0900

    Update student_form.rb

--- app/forms/teacher/student_form.rb
+++ app/forms/teacher/student_form.rb
@@ -10,4 +10,42 @@ class Teacher::StudentForm
     @student_member.build_home_address unless @student_member.home_address
     @student_member.build_parents_address unless @student_member.parents_address
   end
+
+  def assign_attributes(params = {})
+    @params = params
+    student_member.assign_attributes(student_member_params)
+    student_member.home_address.assign_attributes(home_address_params)
+    student_member.parents_address.assign_attributes(parents_address_params)
+  end
+
+  private def student_member_params
+    @params.require(:student_member).permit(
+      :student_number,:family_name,:given_name,
+      :family_name_kana,:given_name_kana,:gender,:email,:grade,
+      :a_class,:password,:emergency_contact,
+      :telephone_number,:homeroom_teacher,
+      :start_date,:graduation_date,:suspended)
+  end
+
+  private def home_address_params
+    @params.require(:home_address).permit(
+      :postal_code,:prefecture,:city,
+      :address1,:address2
+    )
+  end
+
+  private def parents_address_params
+    @params.require(:parents_address).permit(
+      :postal_code,:prefecture,:city,
+      :address1,:address2
+    )
+  end
+
+  def save
+    ActiveRecord::Base.transaction do
+      student_member.save!
+      student_member.home_address.save!
+      student_member.parents_address.save!
+    end
+  end
 end
