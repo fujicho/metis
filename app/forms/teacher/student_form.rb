172d5ae873b20d2eb405c7fa5f5e81b00ac1eb6b
fujicho <nnnn.sohei.fujita@gmail.com>
2020-12-20 14:13:41 +0900

    fix a typo

--- app/forms/teacher/student_form.rb
+++ app/forms/teacher/student_form.rb
@@ -7,7 +7,7 @@ class Teacher::StudentForm
   def initialize(student_member = nil)
     @student_member = student_member
     @student_member ||= StudentMember.new(gender: "male")
-    @student_member.build_home_address unless @student_member.build_home_address
-    @student_member.build_parents_address unless @student_member.build_parents_address
+    @student_member.build_home_address unless @student_member.home_address
+    @student_member.build_parents_address unless @student_member.parents_address
   end
 end
