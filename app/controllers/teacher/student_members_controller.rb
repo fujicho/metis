4de158436e8a1b00c4cc28f66eecd33eab114118
fujicho <nnnn.sohei.fujita@gmail.com>
2020-12-26 19:30:48 +0900

    Update student_members_controller.rb

--- app/controllers/teacher/student_members_controller.rb
+++ app/controllers/teacher/student_members_controller.rb
@@ -16,20 +16,20 @@ class Teacher::StudentMembersController < Teacher::Base
   end
 
   def create
-    @student_member = StudentMember.new(teacher_student_params)
-    if @student_member.save
-      flash.notice = "学生アカウントを新規登録しました。"
-      redirect_to :teacher_student_members
+    @student_form = Teacher::StudentForm.new
+    @student_form.assign_attributes(params[:form])
+    if @student_form.save
+      redirect_to action: "index"
     else
-      render action: "new"
+      redirect_to action: "new"
     end
   end
 
   def update
-    @student_member = StudentMember.find(params[:id])
-    @student_member.assign_attributes(teacher_student_params)
-    if @student_member.save
-      redirect_to :teacher_student_members
+    @student_form = Teacher::StudentForm.new(StudentMember.find(params[:id]))
+    @student_form.assign_attributes(params[:form])
+    if @student_form.save
+      redirect_to action: "index"
     else
       render action: "edit"
     end
@@ -37,14 +37,7 @@ class Teacher::StudentMembersController < Teacher::Base
 
   def destroy
     student_member = StudentMember.find(params[:id])
-    staff_member.destroy!
+    student_member.destroy!
     redirect_to :teacher_student_members
-  end
-
-  private def teacher_student_params
-    params.require(:student_member)
-      .permit(:student_number,:family_name,:given_name,
-        :family_name_kana,:given_name_kana,:gender,:email,:grade,:a_class,:password,:emergency_contact,
-        :telephone_number,:homeroom_teacher,:start_date,:graduation_date,:suspended,:prefecture)
   end
 end
