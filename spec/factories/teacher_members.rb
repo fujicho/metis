42a06a14d8c2f873c5ea84b9c48c5f73a53031f5
fujicho <nnnn.sohei.fujita@gmail.com>
2020-12-26 19:31:29 +0900

    Update teacher_members.rb

--- spec/factories/teacher_members.rb
+++ spec/factories/teacher_members.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     given_name{"太郎"}
     family_name_kana{"ヤマダ"}
     given_name_kana{"タロウ"}
-    password{"pw"}
+    password{"password"}
     start_date{Date.yesterday}
     end_date{nil}
     suspended{false}
