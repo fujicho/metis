f47221f4556d5c312f36ce04cab9c5850adc5f82
fujicho <nnnn.sohei.fujita@gmail.com>
2021-01-02 17:34:07 +0900

    change name

--- spec/features/teacher/student_member_management_spec.rb
+++ spec/features/teacher/student_member_management_spec.rb
@@ -62,18 +62,20 @@ feature "教職員による生徒管理" do
     click_link "学生管理"
     first("#container").click_link "編集"
 
-    within("fieldset#student-fields") do
-      fill_in "メールアドレス", with: "test@example.com"
-    end
+    # within("fieldset#student-fields") do
+      fill_in "form_student_member_email", with: "testtest@example.com"
+    # end
 
     within("fieldset#home-address-fields")do
-      fill_in "郵便番号", with: "1111111"
+      fill_in "郵便番号", with: "1000000"
     end
 
     click_button "更新"
 
     student_member.reload
-    expect(student_member.email).to eq("test@example.com")
-    expect(student_member.home_address.postal_code).to eq("1111111")
+
+    # student = StudentMember.order(:id).last
+    expect(student_member.email).to eq("testtest@example.com")
+    expect(student_member.home_address.postal_code).to eq("1000000")
   end
 end
