061bf51e320cbb2b6f080a86daca071266ef2b69
fujicho <nnnn.sohei.fujita@gmail.com>
2021-01-06 14:01:16 +0900

    fix a typo(space)

--- app/javascript/teacher/student_member_form.js
+++ app/javascript/teacher/student_member_form.js
@@ -1,11 +1,11 @@
 function toggle_home_address_fields() {
-  const checked = $("input#form_inputs_home_address").prop("checked");
+  const checked = $("input#form_inputs_home_address").prop("checked");
   $("fieldset#home-address-fields input").prop("disabled", !checked);
   $("fieldset#home-address-fields select").prop("disabled", !checked);
 }
 
 function toggle_parents_address_fields() {
-  const checked = $("input#form_inputs_parents_address").prop("checked");
+  const checked = $("input#form_inputs_parents_address").prop("checked");
   $("fieldset#parents-address-fields input").prop("disabled", !checked);
   $("fieldset#parents-address-fields select").prop("disabled", !checked);
 }
