b3e3dd0b645d8c51f0fde616d35ad7fd2914e75b
fujicho <nnnn.sohei.fujita@gmail.com>
2021-01-10 14:57:08 +0900

    Update student_search_form.rb

--- app/forms/teacher/student_search_form.rb
+++ app/forms/teacher/student_search_form.rb
@@ -4,4 +4,23 @@ class Teacher::StudentSearchForm
   attr_accessor :family_name_kana, :given_name_kana,
     :birth_year, :birth_month, :gender, :grade, :a_class,
     :address_type, :prefecture, :city
+
+  def search
+    rel = StudentMember
+
+    if family_name_kana.present?
+      rel = rel.where(family_name_kana: family_name_kana)
+    end
+
+    if given_name_kana.present?
+      rel = rel.where(given_name_kana: given_name_kana)
+    end
+
+    rel = rel.where(birth_year: birth_year) if birth_year.present?
+    rel = rel.where(birth_year: birth_month) if birth_month.present?
+    rel = rel.where(grade: grade) if grade.present?
+    rel = rel.where(a_class: a_class) if a_class.present?
+
+    rel.order(:grade, :a_class, :family_name_kana, :given_name_kana, :a_class)
+  end
 end
