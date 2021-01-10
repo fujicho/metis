5e9d5c0943033d9a8f409517e8a613032e3ce281
fujicho <nnnn.sohei.fujita@gmail.com>
2021-01-10 10:50:02 +0900

    Update student_search_form.rb

--- app/forms/teacher/student_search_form.rb
+++ app/forms/teacher/student_search_form.rb
@@ -2,5 +2,6 @@ class Teacher::StudentSearchForm
   include ActiveModel::Model
 
   attr_accessor :family_name_kana, :given_name_kana,
-    :birth_year, :birth_month, :gender, :grade, :a_class
+    :birth_year, :birth_month, :gender, :grade, :a_class,
+    :address_type, :prefecture, :city
 end
