1ce27824206ac90f6803ff6cb91de76c1fe0c189
fujicho <nnnn.sohei.fujita@gmail.com>
2021-01-18 06:57:17 +0900

    Update auto_login_spec.rb

--- spec/requests/student/auto_login_spec.rb
+++ spec/requests/student/auto_login_spec.rb
@@ -12,10 +12,26 @@ describe "次回から自動でログインする" do
           remember_me: "0"
         }
       }
+    expect(session).to have_key(:student_member_id)
+    expect(response.cookies).not_to have_key("student_member_id")
+  end
 
-  expect(session).to have_key(:student_member_id)
-  expect(response.cookies).not_to have_key("student_member_id")
+  example "チェックボックスをオンにした場合" do
+    post student_session_url,
+    params: {
+      student_login_form: {
+        student_number: student_member.student_number,
+        password: "password",
+        remember_me: "1"
+      }
+    }
+    expect(session).not_to have_key(:student_member_id)
+    expect(response.cookies["student_member_id"]).to match(/[0-9a-f]{40}\z/)
 
+    cookies = response.request.env["action_dispatch.cookies"]
+      .instance_variable_get(:@set_cookies)
+
+    expect(cookies["student_member_id"][:expires]).to be > 19.years.from_now
   end
 end
 
