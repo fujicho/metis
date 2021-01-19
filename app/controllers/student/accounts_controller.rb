5c47d0ca7ebd4df53d31d350f8e41b91fdb53f50
fujicho <nnnn.sohei.fujita@gmail.com>
2021-01-19 14:26:59 +0900

    Update accounts_controller.rb

--- app/controllers/student/accounts_controller.rb
+++ app/controllers/student/accounts_controller.rb
@@ -2,4 +2,8 @@ class Student::AccountsController < Student::Base
   def show
     @student_member = current_student_member
   end
+
+  def edit
+    @student_form = Student::AccountForm(current_student_member)
+  end
 end
