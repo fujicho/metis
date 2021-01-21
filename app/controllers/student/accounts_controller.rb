28806d9b46cb80da519b7458d63cd5efd38a1141
fujicho <nnnn.sohei.fujita@gmail.com>
2021-01-21 20:36:57 +0900

    Update accounts_controller.rb

--- app/controllers/student/accounts_controller.rb
+++ app/controllers/student/accounts_controller.rb
@@ -16,4 +16,14 @@ class Student::AccountsController < Student::Base
       render action: "edit"
     end
   end
+
+  def confirm
+    @student_form = Student::AccountForm.new(current_student_member)
+    @student_form.assign_attributes(params[:form])
+    if @student_form.valid?
+      render acotion: "confirm"
+    else
+      render action: "edit"
+    end
+  end
 end
