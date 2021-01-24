896550acba8b17c013d5a0017080d55386b68b5a
fujicho <nnnn.sohei.fujita@gmail.com>
2021-01-24 15:16:00 +0900

    rename

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
       resources :student_events, only: [:index]
       resource :account, except: [ :new, :create, :destroy ]
       resource :password, only: [ :show, :edit, :update]
-      resource :message, only: [ :new, :create, ] do
+      resource :board_message, only: [ :new, :create, ] do
         post :confirm, on: :collection
       end
     end
