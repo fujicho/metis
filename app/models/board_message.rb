678f694dbf34172bee5342fdfa48459a261ac77a
fujicho <nnnn.sohei.fujita@gmail.com>
2021-01-24 14:45:25 +0900

    Update board_message.rb

--- app/models/board_message.rb
+++ app/models/board_message.rb
@@ -1,5 +1,9 @@
 class BoardMessage < ApplicationRecord
   belongs_to :teacher_member
 
-  
+  BOARD_TAGS = %w(文系 理系 全体)
+
+  validates :subject, presence: true, length: {maximum: 80}
+  validates :body, presence: true, length: {maximum: 80}
+  validates :tag, inclusion: { in: BOARD_TAGS, presence: true }
 end
