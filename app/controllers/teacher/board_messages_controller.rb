e40e2d14305da127f2162d88e3189cbbbd832575
fujicho <nnnn.sohei.fujita@gmail.com>
2021-01-25 20:53:03 +0900

    Update board_messages_controller.rb

--- app/controllers/teacher/board_messages_controller.rb
+++ app/controllers/teacher/board_messages_controller.rb
@@ -6,8 +6,19 @@ class Teacher::BoardMessagesController < Teacher::Base
   def new
     @board_message = BoardMessage.new
   end
+  
+  def confirm
+    @board_message = BoardMessage.new(board_message_params)
+    @board_message.teacher_member = current_teacher_member
+    if @board_message.valid?
+      render action: "confirm"
+    else
+      render action: "new"
+    end
+  end
 
   private def board_message_params
     params.require(:board_message).permit(:subject, :body, :tag)
   end
+
 end
