d3b6d657d4be2d4f71c877d17540b80e5f1086ac
fujicho <nnnn.sohei.fujita@gmail.com>
2021-02-02 10:07:58 +0900

    fix a typo

--- app/models/board_message.rb
+++ app/models/board_message.rb
@@ -4,6 +4,6 @@ class BoardMessage < ApplicationRecord
   BOARD_TAGS = %w(全体 文系 理系)
 
   validates :subject, presence: true, length: {maximum: 80}
-  validates :body, presence: true, length: {maximum: 80}
+  validates :body, presence: true, length: {maximum: 800}
   validates :tag, inclusion: { in: BOARD_TAGS, presence: true }
 end
