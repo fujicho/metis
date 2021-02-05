60e9828421bef0965f3488c46f57e8d3ebcb8f57
fujicho <nnnn.sohei.fujita@gmail.com>
2021-02-05 21:33:51 +0900

    Update top_controller.rb

--- app/controllers/teacher/top_controller.rb
+++ app/controllers/teacher/top_controller.rb
@@ -2,7 +2,7 @@ class Teacher::TopController < Teacher::Base
   skip_before_action :authorize
 
   def index
-    @board_messages = BoardMessage.order(created_at: :desc).limit(5)
+    @board_messages = BoardMessage.order(created_at: :desc).limit(5).includes(:teacher_member)
     if current_teacher_member
       render action: "dashboard"
     else
