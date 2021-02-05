3584d06bc4e5295ddf48184dfd963251ebac6067
fujicho <nnnn.sohei.fujita@gmail.com>
2021-02-05 21:27:41 +0900

    Create board_message_form.rb

---
+++ app/forms/teacher/board_message_form.rb
@@ -0,0 +1,23 @@
+class Teacher::BoardMessageForm
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  BOARD_TAGS = %w(全体 文系 理系)
+
+  attr_accessor :teacher_member, :board_message
+  delegate :persisted?, :valid?, to: :board_message
+
+  attribute :subject, :string
+  attribute :body, :string
+  attribute :tag, :string
+
+  validates :subject, presence: true, length: {maximum: 80}
+  validates :body, presence: true, length: {maximum: 800}
+  validates :tag, inclusion: { in: BOARD_TAGS, presence: true }
+
+  def initialize(teacher_member, board_message, params = {})
+    @teacher_member = teacher_member
+    @board_message = board_message
+    super(params)
+  end
+end
