f0bbfc8fbf8dee24392d75e6921f4f9b997be305
fujicho <nnnn.sohei.fujita@gmail.com>
2021-02-23 20:54:41 +0900

    掲示板編集機能をフォームオブジェクトに換装

--- app/controllers/teacher/board_messages_controller.rb
+++ app/controllers/teacher/board_messages_controller.rb
@@ -39,26 +39,52 @@ class Teacher::BoardMessagesController < Teacher::Base
     @board_message = BoardMessage.find(params[:id])
   end
 
+  # def edit
+  #   @board_message = BoardMessage.find(params[:id])
+  # end
   def edit_confirm
     @board_message = BoardMessage.find(params[:board_message_id])
-    @board_message.update_attributes(board_message_params)
-    if @board_message.valid?
+    @board_form = Teacher::BoardMessageForm.new(current_teacher_member, @board_message, board_message_params)
+    if @board_form.valid?
       render action: "edit_confirm"
-    else
-      render action: "new"
-    end
-  end
-  
-  def update
-    @board_message = BoardMessage.find(params[:id])
-    @board_message.update_attributes(board_message_params)
-    if params[:commit]
-      @board_message.save
-      redirect_to :teacher_root
     else
       render action: "edit"
     end
   end
+
+  def update
+    @board_message = BoardMessage.find(params[:id])
+    @board_form = Teacher::BoardMessageForm.new(current_teacher_member, @board_message, board_message_params)
+    if params[:commit]
+      if @board_form.update(board_message_params)
+        redirect_to :teacher_root
+      else
+        render action: "edit"
+      end
+    else
+      render action: "edit"
+    end
+  end
+  # def edit_confirm
+  #   @board_message = BoardMessage.find(params[:board_message_id])
+  #   @board_message.update_attributes(board_message_params)
+  #   if @board_message.valid?
+  #     render action: "edit_confirm"
+  #   else
+  #     render action: "new"
+  #   end
+  # end
+  
+  # def update
+  #   @board_message = BoardMessage.find(params[:id])
+  #   @board_message.update_attributes(board_message_params)
+  #   if params[:commit]
+  #     @board_message.save
+  #     redirect_to :teacher_root
+  #   else
+  #     render action: "edit"
+  #   end
+  # end
 
   def destroy
     board_message = BoardMessage.find(params[:id])
@@ -67,6 +93,6 @@ class Teacher::BoardMessagesController < Teacher::Base
   end
 
   private def board_message_params
-    params.require(:form).permit(:subject, :body, :tag,:teacher_member)
+    params.require(:form).permit(:subject, :body, :tag)
   end
 end
