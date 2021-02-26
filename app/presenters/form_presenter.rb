0d7c003fcb4ebd35efc5dcb5f9969e5f07235625
fujicho <nnnn.sohei.fujita@gmail.com>
2021-02-26 10:00:45 +0900

    Update form_presenter.rb

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -1,13 +1,23 @@
 class FormPresenter
   include HtmlBuilder
-
+  
   attr_reader :form_builder, :view_context
   delegate :label, :text_field, :date_field, :password_field,
-    :check_box, :radio_button, :text_area, :object, to: :form_builder
-
+  :check_box, :radio_button, :text_area, :object, to: :form_builder
+  
   def initialize(form_builder,view_context)
     @form_builder = form_builder
     @view_context = view_context
+  end
+  
+  def error_messages_for(name)
+    markup do |m|
+      object.errors.full_messages_for(name).each do |message|
+        m.div(class: "error-messsage") do |m|
+          m.text message
+        end
+      end
+    end
   end
 
   def text_field_block(name, label_text, options= {})
@@ -26,15 +36,6 @@ class FormPresenter
     end
   end
 
-  def error_messages_for(name)
-    markup do |m|
-      object.errors.full_messages_for(name).each do |message|
-        m.div(class: "error-messsage") do |m|
-          m.text message
-        end
-      end
-    end
-  end
 
   def password_field_block(name,label_text, options = {})
     markup(:div, class: "input-block") do |m|
