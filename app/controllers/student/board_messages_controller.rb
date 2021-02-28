9a7a158597aa0795ebccce4563a566172c21a030
fujicho <nnnn.sohei.fujita@gmail.com>
2021-02-28 15:43:29 +0900

    ページネーションの追加

--- app/controllers/student/board_messages_controller.rb
+++ app/controllers/student/board_messages_controller.rb
@@ -1,7 +1,7 @@
 class Student::BoardMessagesController < Student::Base
 
   def index
-    @board_messages = BoardMessage.order(created_at: :desc).where(tag: [current_student_member.bunri, "全体"]).limit(5).includes(:teacher_member)
+    @board_messages = BoardMessage.order(created_at: :desc).where(tag: [current_student_member.bunri, "全体"]).limit(5).includes(:teacher_member).page(params[:page])
     render action: "index"
   end
 end
