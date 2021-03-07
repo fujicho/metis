8c32035f35ae14ebec05dffc96d4467386e59130
fujicho <nnnn.sohei.fujita@gmail.com>
2021-03-07 16:51:01 +0900

    booksresoucesをネストから外す,ルーティング名を変更

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,10 @@ Rails.application.routes.draw do
         post :confirm, on: :collection
         patch :edit_confirm
       end
+      resources :books, except: [:show]
+      #主に参考書、大学別過去問カテゴリの登録編集削除を行う
       resources :question do
-        resources :books
-        #主に参考書、大学別過去問カテゴリの登録編集削除を行う
-        resources :articles
+        resources :answers, only: [:index, :new, :create, :edit,:update]
         #主に質問掲示板絵の投稿、リプライ、削除などを行う。
       end
     end
