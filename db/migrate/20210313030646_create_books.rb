4d850f33ab47d1a85c71f2bb9daf8a8a4f42260e
fujicho <nnnn.sohei.fujita@gmail.com>
2021-03-14 12:02:32 +0900

    カラムの追加

--- db/migrate/20210313030646_create_books.rb
+++ db/migrate/20210313030646_create_books.rb
@@ -4,6 +4,7 @@ class CreateBooks < ActiveRecord::Migration[6.0]
       t.string :book_name , null: false #問題集の題名または過去問の大学名
       t.string :book_subject, null: false #科目
       t.integer :book_year #過去問の年数または問題集の発行年度
+      t.integer :faculty_name #過去問の学部名
       t.string :type, null: false #過去問か問題集か
 
       t.timestamps
