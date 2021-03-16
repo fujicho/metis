8ad3a19959da877825273cb65cfc4e83e5164508
fujicho <nnnn.sohei.fujita@gmail.com>
2021-03-16 11:18:23 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2021_03_13_030646) do
     t.string "book_name", null: false
     t.string "book_subject", null: false
     t.integer "book_year"
+    t.integer "faculty_name"
+    t.string "type", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["book_name"], name: "index_books_on_book_name"
