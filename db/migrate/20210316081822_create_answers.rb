b2090983a03c48690d9b0217a96375a021b352f3
fujicho <nnnn.sohei.fujita@gmail.com>
2021-03-16 18:14:52 +0900

    Update 20210316081822_create_answers.rb

--- db/migrate/20210316081822_create_answers.rb
+++ db/migrate/20210316081822_create_answers.rb
@@ -1,8 +1,15 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
+      t.references :question, null: false
+      t.references :teacher_member
+      t.references :student_memner
+      t.string :body
 
       t.timestamps
     end
+    add_foreign_key :answers, :question
+    add_foreign_key :answers, :teacher_member
+    add_foreign_key :answers, :student_member
   end
 end
