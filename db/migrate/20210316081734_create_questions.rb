04735f5ed58e7f35d01a4e3852e4dee637491f6d
fujicho <nnnn.sohei.fujita@gmail.com>
2021-03-16 18:14:48 +0900

    Update 20210316081734_create_questions.rb

--- db/migrate/20210316081734_create_questions.rb
+++ db/migrate/20210316081734_create_questions.rb
@@ -1,7 +1,11 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-
+      t.references :student_member, null: false
+      t.references :book, null: false
+      t.string :title, null: false
+      t.string :body, null: false
+      t.string :subject
       t.timestamps
     end
   end
