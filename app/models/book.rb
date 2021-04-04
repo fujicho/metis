f11419d472fe2350368b65dd44f99e9876d0ed78
fujicho <nnnn.sohei.fujita@gmail.com>
2021-04-04 10:54:21 +0900

    add SUBJECT_NAMES

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,8 @@ class Book < ApplicationRecord
   BOOK_YEAR = (1950..this_year).to_a
 
   validates :book_name, :book_subject, presence: true
+
+  SUBJECT_NAMES = %w(
+    英語 数学 物理 化学 現代文 古典 世界史 日本史 地理
+  )
 end
