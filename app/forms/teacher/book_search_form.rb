a9f733d96e2471388c3ff995588132f9dbcb2fae
fujicho <nnnn.sohei.fujita@gmail.com>
2021-04-06 20:37:37 +0900

    Create book_search_form.rb

---
+++ app/forms/teacher/book_search_form.rb
@@ -0,0 +1,28 @@
+class Teacher::BookSearchForm
+  include ActiveModel::Model
+
+  attr_accessor :book_name, :book_subject, :book_year,
+    :faculty_name, :type
+
+  def search
+    rel = Book
+
+    if book_name.present?
+      rel = rel.where(book_name: book_name)
+    end
+
+    if book_subject.present?
+      rel = rel.where(book_subject: book_subject)
+    end
+
+    if book_year.present?
+      rel = rel.where(book_year: book_year)
+    end
+
+    if faculty_name.present?
+      rel = rel.where(faculty_name: faculty_name)
+    end
+
+    rel.order(:book_name, :book_year)
+  end
+end
