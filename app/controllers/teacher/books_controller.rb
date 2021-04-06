1bdc04dd9c39d136eb0bc24007b35ec3c4eb8996
fujicho <nnnn.sohei.fujita@gmail.com>
2021-04-06 20:37:27 +0900

    Update books_controller.rb

--- app/controllers/teacher/books_controller.rb
+++ app/controllers/teacher/books_controller.rb
@@ -1,6 +1,13 @@
 class Teacher::BooksController < Teacher::Base
   def index
-    @book_serch_form = Teacher::BookSearchForm.new
-    @books = WorkBook.order(created_at: :desc).all
+    @book_search_form = Teacher::BookSearchForm.new(search_params)
+    @books = @book_search_form.search.page(params[:page])
+  end
+
+  private def search_params
+    params[:search]&.permit([
+      :book_name, :book_subject, :type,
+      :book_year, :faculty_name
+    ])
   end
 end
