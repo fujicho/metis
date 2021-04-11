eb211815aa454aef14c5fe3c637ab726f4c141b2
fujicho <nnnn.sohei.fujita@gmail.com>
2021-04-11 20:56:48 +0900

    WorkBook,PastBookの検索切り替えの実装

--- app/forms/teacher/book_search_form.rb
+++ app/forms/teacher/book_search_form.rb
@@ -1,27 +1,37 @@
 class Teacher::BookSearchForm
   include ActiveModel::Model
-
+  
   attr_accessor :book_name, :book_subject, :book_year, :type
+  
+  def rel_where 
+    if book_name.present?
+      rel = rel.where('book_name LIKE ?', "%#{book_name}%")
+    end
+    
+    if book_subject.present?
+      rel = rel.where(book_subject: book_subject)
+    end
+    
+    if book_year.present?
+      rel = rel.where(book_year: book_year)
+    end
+  end
 
   def search
     rel = Book
 
-    if book_name.present?
-      rel = rel.where('book_name LIKE ?', "%#{book_name}%")
+    if type.present?
+      case type
+      when "work"
+        rel = WorkBook
+        rel_where
+      when "past"
+        rel = PastBook
+        rel_where
+      else
+        rel.order(:book_name, :book_year)
+      end
     end
-
-    if book_subject.present?
-      rel = rel.where(book_subject: book_subject)
-    end
-
-    if book_year.present?
-      rel = rel.where(book_year: book_year)
-    end
-
-    # if faculty_name.present?
-    #   rel = rel.where(faculty_name: faculty_name)
-    # end
-
     rel.order(:book_name, :book_year)
   end
 end
