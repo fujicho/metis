3ba456f17f767d610fe40ad75f4628817bef8df9
fujicho <nnnn.sohei.fujita@gmail.com>
2021-04-12 12:41:57 +0900

    Update books_controller.rb

--- app/controllers/teacher/books_controller.rb
+++ app/controllers/teacher/books_controller.rb
@@ -4,6 +4,22 @@ class Teacher::BooksController < Teacher::Base
     @books = @book_search_form.search.page(params[:page])
   end
 
+  def new
+    @book_form = Teacher::BookForm.new
+  end
+
+  def create
+    @book_form = Teacher::BookForm.new
+    @book_form.assign_attributes(params[:form])
+    if @book_form.save
+      redirect_to action: "index"
+    else
+      redirect_to action: "new"
+    end
+  end
+
+
+
   private def search_params
     params[:search]&.permit([
       :book_name, :book_subject, :type,
