c6dc59e169450e98ed2680997e89cab968027518
fujicho <nnnn.sohei.fujita@gmail.com>
2021-04-17 20:04:23 +0900

    Update book_presenter.rb

--- app/presenters/book_presenter.rb
+++ app/presenters/book_presenter.rb
@@ -2,6 +2,8 @@ class BookPresenter < ModelPresenter
   def book_name
     if object.instance_of?(PastBook)
       object.book_name + " " + object.book_subject + "(" + object.book_year.to_s + ")"
+    elsif object.instance_of?(WorkBook)
+      object.book_name
     else
       object.book_name
     end
