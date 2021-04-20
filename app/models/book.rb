f4e7d83aba40fed1b2b15cac84528b4da6da8b3e
fujicho <nnnn.sohei.fujita@gmail.com>
2021-04-21 08:30:56 +0900

    Update book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,12 @@
 class Book < ApplicationRecord
+  validates :book_name, :book_subject, presence: true
+
   require "date"
   this_year = Date.today.year
   years = (1950..this_year).to_a
   BOOK_YEAR = years << "不明"
 
 
-  validates :book_name, :book_subject, presence: true
 
   SUBJECT_NAMES = %w(
     英語 数学 物理 化学 現代文 古典 世界史 日本史 地理
