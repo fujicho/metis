8fc3a8f5e7120de11c9ee6882881d7eb40a78ad5
fujicho <nnnn.sohei.fujita@gmail.com>
2021-04-25 13:17:21 +0900

    add validates

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,7 @@ class Question < ApplicationRecord
   belongs_to :book, dependent: :destroy
   belongs_to :student_member, dependent: :destroy
   has_many :answers, dependent: :restrict_with_exception
+
+  validates :title, :body, :subject, presence: :true
+
 end
