eeb7c440bfa9570d2b8d45dd260c83a0124274ff
fujicho <nnnn.sohei.fujita@gmail.com>
2021-05-05 12:23:11 +0900

    Create book_search_form.rb

---
+++ app/forms/student/book_search_form.rb
@@ -0,0 +1,28 @@
+class Student::BookSearchForm
+  include ActiveModel::Model
+  
+  attr_accessor :book_name, :book_subject, :book_year, :book_type
+
+  def search
+
+    rel = Book
+
+    if book_type.present?
+      rel = rel.where(book_type: book_type)
+    end
+
+    if book_name.present?
+      rel = rel.where('book_name LIKE ?', "%#{book_name}%")
+    end
+    
+    if book_subject.present?
+      rel = rel.where(book_subject: book_subject)
+    end
+    
+    if book_year.present?
+      rel = rel.where(book_year: book_year)
+    end
+
+    rel.order(:book_name, :book_year)
+  end
+end
