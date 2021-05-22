0637d69aabab85d4c7469ddb168c8d665d5fe91c
fujicho <nnnn.sohei.fujita@gmail.com>
2021-05-22 12:14:59 +0900

    add a validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,6 @@ class Answer < ApplicationRecord
   belongs_to :question, dependent: :destroy
   belongs_to :teacher_member
   belongs_to :student_member
+
+  validates :body, presence: true length: { maxmmum: 800 }
 end
