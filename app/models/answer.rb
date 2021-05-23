1cdaa34ce250ffa9a868328a14cd1f6ddbd83133
fujicho <nnnn.sohei.fujita@gmail.com>
2021-05-23 12:09:36 +0900

    add optional and fix typo

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ApplicationRecord
   belongs_to :question, dependent: :destroy
-  belongs_to :teacher_member
-  belongs_to :student_member
+  belongs_to :teacher_member, optional: true
+  belongs_to :student_member, optional: true
 
-  validates :body, presence: true length: { maxmmum: 800 }
+  validates :body, presence: true, length: { maximum: 800 }
 end
