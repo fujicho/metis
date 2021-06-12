02b38bc3814e4591258f5c717ba97bd422340a26
fujicho <nnnn.sohei.fujita@gmail.com>
2021-06-12 16:20:02 +0900

    add tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "値の正規化" do
+    example "titleの前後の空白を除去" do
+      question = create(:question, title: " 2題目がわかりません。 ")
+      expect(question.title).to eq("2題目がわかりません。")
+    end
+
+    example "titleの全角数字を半角数字に変換" do
+      question = create(:question, title: "２題目がわかりません。")
+      expect(question.title).to eq("2題目がわかりません。")
+    end
+
+
+  end
 end
