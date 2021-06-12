0c764410e416b266bbd01d7393f14a6eab0788b0
fujicho <nnnn.sohei.fujita@gmail.com>
2021-06-12 14:54:00 +0900

    validationtestの追加

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  describe "値の正規化"
+  describe "値の正規化" do
     example "book_nameに含まれる前後の空白を除去" do
       book = create(:book, book_name: " ハイレベル理系数学 ")
       expect(book.book_name).to eq("ハイレベル理系数学")
@@ -16,4 +16,17 @@ RSpec.describe Book, type: :model do
       book = create(:book, book_name: "理系数学のプラチカ１A２B")
       expect(book.book_name).to eq("理系数学のプラチカ1A2B")
     end
+  end
+
+  describe "バリデーション" do
+    example "book_nameが空欄の場合無効" do
+      book = build(:book, book_name: "")
+      expect(book).not_to be_valid
+    end
+
+    example "book_subjectが空欄の場合無効" do
+      book = build(:book, book_subject: "")
+      expect(book).not_to be_valid
+    end
+  end
 end
