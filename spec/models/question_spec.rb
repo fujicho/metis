90a056714244cd4c9ef5d673176cbdf4a5288f1e
fujicho <nnnn.sohei.fujita@gmail.com>
2021-06-13 11:48:13 +0900

    add tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -12,6 +12,21 @@ RSpec.describe Question, type: :model do
       expect(question.title).to eq("2題目がわかりません。")
     end
 
+    example "titleに含まれる半角カナを全角カナに変換" do
+      question = create(:question, title: "方向ﾍﾞｸﾄﾙに関しての質問です。")
+      expect(question.title).to eq("方向ベクトルに関しての質問です。")
+    end
+  end
 
+  describe "バリデーション" do
+    example "titleが空欄の場合無効" do
+      question = build(:question, title: "")
+      expect(question).not_to be_valid
+    end
+
+    example "bodyが空欄の場合無効" do
+      question = build(:question, body: "")
+      expect(question).not_to be_valid
+    end
   end
 end
