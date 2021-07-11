16f0dae635043320adde53a79cb1962875269274
fujicho <nnnn.sohei.fujita@gmail.com>
2021-07-11 11:34:03 +0900

    Create header.vue

---
+++ app/javascript/packs/components/header.vue
@@ -0,0 +1,50 @@
+<template>
+  <header>
+    <h1>
+      <a href="/">Metista</a>
+    </h1>
+    <nav>
+      <ul>
+        <v-btn href="/account" text color="primary" dark>アカウント</v-btn>
+        <v-btn href="/session" text data-method="delete" color="primary" dark>ログアウト</v-btn>
+        <v-spacer></v-spacer>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style>
+header {
+    width: 100%;
+    padding: 10px 4% 10px;
+    background-color: #2196F3;
+    position: fixed;
+    top: 0;
+    display: flex;
+    align-items: center;
+  }
+h1
+  {
+    margin: 0; padding: 0;
+    font-size: 20px;
+  }
+a {
+  text-decoration: none;
+  color: #ffffff;
+  }
+ul {
+  list-style: none;
+  margin: 0;
+  display: flex;
+}
+li {
+  margin: 0 0 0 15px;
+  font-size: 14px;
+}
+nav {
+  margin: 0 0 0 auto;
+  }
+</style>
