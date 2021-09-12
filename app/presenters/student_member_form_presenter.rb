ae2f5721dbece368739471679067a45b65658014
fujicho <nnnn.sohei.fujita@gmail.com>
2021-09-12 17:41:54 +0900

    full_name_block_kanaの作成とクラスの付与

--- app/presenters/student_member_form_presenter.rb
+++ app/presenters/student_member_form_presenter.rb
@@ -1,10 +1,20 @@
 class StudentMemberFormPresenter < FormPresenter
 
   def full_name_block(name1, name2, label_text, options = {})
-    markup(:div, class: "my-2 col-3") do |m|
+    markup(:div, class: "col-10 my-2 row") do |m|
       m << decorated_label(name1, label_text, class: "form-label")
-      m << text_field( name1, class: "form-control my-2")
-      m << text_field( name2, class: "form-control my-2")
+      m << text_field( name1, class: "form-control my-2 mx-3 col", placeholder: "性")
+      m << text_field( name2, class: "form-control my-2 mx-3 col", placeholder: "名")
+      m << error_messages_for(name1)
+      m << error_messages_for(name2)
+    end
+  end
+
+  def full_name_block_kana(name1, name2, label_text, options = {})
+    markup(:div, class: "col-10 my-2 row") do |m|
+      m << decorated_label(name1, label_text, class: "form-label")
+      m << text_field( name1, class: "form-control my-2 mx-3 col", placeholder: "セイ")
+      m << text_field( name2, class: "form-control my-2 mx-3 col", placeholder: "メイ")
       m << error_messages_for(name1)
       m << error_messages_for(name2)
     end
@@ -18,7 +28,7 @@ class StudentMemberFormPresenter < FormPresenter
   end
 
   def date_field_block(name, label_text, options = {})
-    markup(:div) do |m|
+    markup(:div, class: "col-6") do |m|
       m << decorated_label(name, label_text, options)
       m << date_field(name,options)
       m << error_messages_for(name)
@@ -35,11 +45,11 @@ class StudentMemberFormPresenter < FormPresenter
 
   def gender_field_block
     markup(:div, class: "radio-buttons mb-2") do |m|
-      m << label(:gender,"性別")
-      m << radio_button(:gender, "male")
-      m << label(:gender, "男性")
-      m << radio_button(:gender, "female")
-      m << label(:gender, "女性")
+      m << label(:gender, "性別")
+      m << radio_button(:gender, "male", class: "form-check-input mx-2")
+      m << label(:gender_male, "男性", class: "form-check-label")
+      m << radio_button(:gender, "female", class: "form-check-input mx-2")
+      m << label(:gender_female, "女性")
     end
   end
 end
