822cf08998447fc92adff0e17d99320536cf5f6c
fujicho <nnnn.sohei.fujita@gmail.com>
2021-09-12 11:38:08 +0900

    classの付与

--- app/presenters/student_member_form_presenter.rb
+++ app/presenters/student_member_form_presenter.rb
@@ -1,10 +1,10 @@
 class StudentMemberFormPresenter < FormPresenter
 
   def full_name_block(name1, name2, label_text, options = {})
-    markup(:div) do |m|
-      m << decorated_label(name1, label_text, options)
-      m << text_field( name1, options)
-      m << text_field( name2, options)
+    markup(:div, class: "my-2 col-3") do |m|
+      m << decorated_label(name1, label_text, class: "form-label")
+      m << text_field( name1, class: "form-control my-2")
+      m << text_field( name2, class: "form-control my-2")
       m << error_messages_for(name1)
       m << error_messages_for(name2)
     end
