2f4cbb51f9b09bf9971bc84fed027d8a89a9e4f6
fujicho <nnnn.sohei.fujita@gmail.com>
2021-09-17 22:19:10 +0900

    add a posted_by_badge method

--- app/presenters/answer_presenter.rb
+++ app/presenters/answer_presenter.rb
@@ -4,4 +4,12 @@ class AnswerPresenter < ModelPresenter
   def posted_at
     "投稿日 " + object.created_at.strftime('%Y/%m/%d')
   end
+
+  def posted_by_badge
+    if object.student_member.present?
+      "学生"
+    else
+      "教師"
+    end
+  end
 end
