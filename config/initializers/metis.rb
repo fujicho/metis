6fc26aef463ab4d4a37cddbb4b5347b4f33d6095
fujicho <nnnn.sohei.fujita@gmail.com>
2021-10-31 10:12:54 +0900

    Update metis.rb

--- config/initializers/metis.rb
+++ config/initializers/metis.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.metis = {
-    teacher: { host: "heroku-metista.heroku.com", path: "" },
+    teacher: { host: "heroku-metista.herokuapp.com", path: "" },
     student: { host: "metis.example.com", path: "mypage"}
   }
 end
