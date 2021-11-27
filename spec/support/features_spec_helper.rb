0fbbdc994985f15c37e4fe760214f871616ff222
fujicho <nnnn.sohei.fujita@gmail.com>
2021-11-27 17:46:27 +0900

    Update features_spec_helper.rb

--- spec/support/features_spec_helper.rb
+++ spec/support/features_spec_helper.rb
@@ -12,4 +12,13 @@ module FeaturesSpecHelper
       click_button "ログイン"
     end
   end
+
+  def login_as_student_member(student_member,password = "password")
+    visit student_login_path
+    within(".login-form") do
+      fill_in "学生ID", with: student_member.student_number
+      fill_in "パスワード", with: password
+      click_button "ログイン"
+    end
+  end
 end
