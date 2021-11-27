348875aa316012689ac11d17a0524a2466fd8db7
fujicho <nnnn.sohei.fujita@gmail.com>
2021-11-27 09:36:45 +0900

    Update student_member_management_spec.rb

--- spec/features/teacher/student_member_management_spec.rb
+++ spec/features/teacher/student_member_management_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-xfeature "教職員による生徒管理" do
+feature "教職員による生徒管理" do
   include FeaturesSpecHelper
   let(:teacher_member) { create(:teacher_member) }
   let!(:student_member) { create(:student_member)}
@@ -11,10 +11,10 @@ xfeature "教職員による生徒管理" do
   end
 
   scenario "教職員が生徒情報、住所を新規登録する" do
-    click_link "学生管理"
-    first("#container").click_link "新規登録"
+    first(".side-nav").click_link "学生管理"
+    click_link "新規登録はこちら"
 
-    within("fieldset#student-fields") do
+    within("#student-info") do
       fill_in "学生番号", with: "99999999"
       fill_in "form_student_member_family_name", with: "藤田"
       fill_in "form_student_member_given_name", with: "広重"
@@ -33,7 +33,7 @@ xfeature "教職員による生徒管理" do
     end
     check "form[inputs_home_address]"
 
-    within("fieldset#home-address-fields") do
+    within("#student-address") do
       fill_in "郵便番号", with: "1111111"
       select "東京都", from: "都道府県"
       fill_in "市区町村", with: "中央区"
@@ -41,7 +41,7 @@ xfeature "教職員による生徒管理" do
       fill_in "建物名、部屋番号等", with: "なんちゃらハイツ101"
     end
     check "form[inputs_parents_address]"
-    within("fieldset#parents-address-fields") do
+    within("#parents-address") do
       fill_in "郵便番号", with: "2222222"
       select "北海道", from: "都道府県"
       fill_in "市区町村", with: "南区"
@@ -60,7 +60,7 @@ xfeature "教職員による生徒管理" do
   end
 
   scenario "教職員が生徒情報、住所を更新する" do
-    click_link "学生管理"
+    first(".side-nav").click_link "学生管理"
     first("#container").click_link "編集"
 
     fill_in "form_student_member_email", with: "test2@example.com"
@@ -80,7 +80,7 @@ xfeature "教職員による生徒管理" do
   scenario "教職員が生徒情報、住所を更新する際、student_memberに適切な値、
     student_member.home_addressに不適切な値を入れた場合、両方とも更新されない" do
     
-    click_link "学生管理"
+    first(".side-nav").click_link "学生管理"
     first("#container").click_link "編集"
 
     fill_in "form_student_member_email", with: "test@example.com"
@@ -98,7 +98,7 @@ xfeature "教職員による生徒管理" do
   end
 
   scenario "教職員がfamily_name_kanaを用いて生徒情報を検索する。" do
-    click_link "学生管理"
+    first(".side-nav").click_link "学生管理"
 
     fill_in "search_family_name_kana", with: "ヤマオカ"
     
@@ -109,9 +109,9 @@ xfeature "教職員による生徒管理" do
 
   scenario "教職員が電話番号を用いて生徒情報を検索する。電話番号フォームからtelephone_number,emergency_contactどちらのカラムも検索することができる。
               本テストはtelephone_numberから" do
-    click_link "学生管理"
+    first(".side-nav").click_link "学生管理"
 
-    fill_in "search_phone_number", with: "98765432100"
+    fill_in "search_phone_number", with: "12345678900"
       
     click_button "検索"
 
@@ -120,7 +120,7 @@ xfeature "教職員による生徒管理" do
 
   scenario "教職員が電話番号を用いて生徒情報を検索する。電話番号フォームからtelephone_number,emergency_contactどちらのカラムも検索することができる。
               本テストはemergency_contactから" do
-    click_link "学生管理"
+    first(".side-nav").click_link "学生管理"
 
     fill_in "search_phone_number", with: "12345678900"
       
@@ -130,7 +130,7 @@ xfeature "教職員による生徒管理" do
   end
 
   scenario "教職員がハイフン入りの電話番号を用いて生徒情報を検索する。(正規化のテスト)" do
-    click_link "学生管理"
+    first(".side-nav").click_link "学生管理"
 
     fill_in "search_phone_number", with: "123-4567-8900"
       
