5fc0ce3ed630eab64a67cd8b98180690ea7574e7
fujicho <nnnn.sohei.fujita@gmail.com>
2021-11-27 10:00:41 +0900

    値の正規化の

--- app/models/board_message.rb
+++ app/models/board_message.rb
@@ -1,9 +1,16 @@
 class BoardMessage < ApplicationRecord
+  include StringNormalizer
+
   belongs_to :teacher_member
 
   BOARD_TAGS = %w(全体 文系 理系)
-
+  
   validates :subject, presence: true, length: {maximum: 80}
   validates :body, presence: true, length: {maximum: 800}
   validates :tag, inclusion: { in: BOARD_TAGS, presence: true }
+  
+  before_validation do
+    self.subject = normalize_as_text(subject)
+    self.body = normalize_as_text(body)
+  end
 end
