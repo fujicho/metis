41c317ba6daa09da46a665078f4c1ab9253880c5
fujicho <nnnn.sohei.fujita@gmail.com>
2021-12-06 19:26:44 +0900

    jqueryをvueに置き換え

--- app/javascript/teacher/student_member_form.js
+++ app/javascript/teacher/student_member_form.js
@@ -1,22 +1,10 @@
-function toggle_home_address_fields() {
-  const checked = $("input#form_inputs_home_address").prop("checked");
-  $("fieldset#home-address-fields input").prop("disabled", !checked);
-  $("fieldset#home-address-fields select").prop("disabled", !checked);
-}
+import Vue from 'vue/dist/vue.esm'
 
-function toggle_parents_address_fields() {
-  const checked = $("input#form_inputs_parents_address").prop("checked");
-  $("fieldset#parents-address-fields input").prop("disabled", !checked);
-  $("fieldset#parents-address-fields select").prop("disabled", !checked);
-}
-
-$(document).on("ready turbolinks:load", () => {
-  toggle_home_address_fields();
-  toggle_parents_address_fields();
-  $("input#form_inputs_home_address").on("click", () => {
-    toggle_home_address_fields();
-  });
-  $("input#form_inputs_parents_address").on("click", () => {
-    toggle_parents_address_fields();
-  });
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    el: '#wrapper',
+    data: {
+      able: true
+    }
+  })
+})
