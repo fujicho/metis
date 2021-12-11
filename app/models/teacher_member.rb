09266c0ad44a9285fa96c6e9f774ed674cc044a3
fujicho <nnnn.sohei.fujita@gmail.com>
2021-12-11 13:29:50 +0900

    バリデーションの追加

--- app/models/teacher_member.rb
+++ app/models/teacher_member.rb
@@ -1,14 +1,15 @@
 class TeacherMember < ApplicationRecord
+  has_one :address, dependent: :destroy
+  has_many :answers, dependent: :destroy
+  
   include StringNormalizer
   include PersonalNameHolder
   include EmailHolder
   include PasswordHolder
 
-  has_one :address, dependent: :destroy
-  has_many :answers, dependent: :destroy
-
   validates :birthday, date: {before: ->(obj) { Date.today }, presence: true}
-  
+  validates :email, uniqueness: true, presence: true
+
   def active?
     !suspended? && start_date <= Date.today &&
     ( end_date.nil? || end_date > Date.today)
