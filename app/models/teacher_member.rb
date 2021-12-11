43f69322a9b36b04230ea2ab678debb5ff330915
fujicho <nnnn.sohei.fujita@gmail.com>
2021-12-11 12:55:39 +0900

    誕生日のバリデーションを追加

--- app/models/teacher_member.rb
+++ app/models/teacher_member.rb
@@ -6,9 +6,9 @@ class TeacherMember < ApplicationRecord
 
   has_one :address, dependent: :destroy
   has_many :answers, dependent: :destroy
-  
-  
 
+  validates :birthday, date: {before: ->(obj) { Date.today }, presence: true}
+  
   def active?
     !suspended? && start_date <= Date.today &&
     ( end_date.nil? || end_date > Date.today)
