f97d429a1f2dec3a32232bb8e3b49c55e870e7b4
fujicho <nnnn.sohei.fujita@gmail.com>
2022-02-06 13:57:49 +0900

    Update student_member_management.rb

--- spec/requests/teacher/student_member_management.rb
+++ spec/requests/teacher/student_member_management.rb
@@ -5,41 +5,39 @@ describe "管理者による職員管理","ログイン前" do
 end
 
 describe "教職員による生徒管理" do
-  let(:teacher_member){ create(:teacher_member)}
-
+  let(:teacher_member) { create(:teacher_member) }
   before do
-    post teacher_session_url
-      params: {
-        teacher_login_form: {
-          email: member0@example.com,
-          password: "pw"
-        }
+    post teacher_session_url, 
+    params: {
+      teacher_login_form: {
+        email: teacher_member.email, 
+        password: "pw"
       }
+    }
   end
 
   describe "新規登録" do
-    let(:params_hash){ attributes_for(:student_member)}
+    let(:params_hash) { attributes_for(:student_member) }
 
     example "生徒一覧にリダイレクト" do
-      post teacher_student_members_url, params:{ student_member: params_hash}
+      post teacher_student_members_url, params: { student_member: params_hash }
       expect(response).to redirect_to(teacher_student_members_url)
     end
 
     example "例外 ActionController::ParameterMissingが発生" do
-      expect { post teacher_student_members_url}.
-        to raise_error(ActionController::ParameterMissing)
+      expect { post teacher_student_members_url }
+        .to raise_error(ActionController::ParameterMissing)
     end
-
   end
 
   describe "更新" do
-    let(:student_member){ create(:student_member)}
-    let(:params_hash){ attributes_for(:student_member)}
+    let(:student_member) { create(:student_member) }
+    let(:params_hash) { attributes_for(:student_member) }
     
     example "suspendedフラグをセットする" do
       params_hash.merge!(suspended: true)
       patch teacher_student_member_url(student_member),
-        params: { student_member: params_hash}
+            params: { student_member: params_hash }
       student_member.reload
       expect(student_member).to be_suspended
     end
@@ -47,9 +45,10 @@ describe "教職員による生徒管理" do
     example "hashed_passwordの書き換え不可" do
       params_hash.delete(:password)
       params_hash.merge!(hashed_password: "abc")
-      expect{ patch teacher_student_member_url(student_member),
-        params: { student_member: params_hash}
-      }.not_to change{ student_member.hashed_password.to_s}
+      expect  do 
+        patch teacher_student_member_url(student_member),
+              params: { student_member: params_hash }
+      end.not_to change { student_member.hashed_password.to_s }
     end
   end
 end
